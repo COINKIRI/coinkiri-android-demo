dd238e0842dbdecca27f2ab4690851de25d1b008
wjdrjs00 <kimjk3409@gmail.com>
2024-05-15 07:37:26 +0900

    #7 ✨ feat : loginViewModel에 sharedPreferences로직 추가

--- app/src/main/java/com/cokiri/coinkiri/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/cokiri/coinkiri/presentation/login/LoginViewModel.kt
@@ -2,6 +2,7 @@ package com.cokiri.coinkiri.presentation.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cokiri.coinkiri.data.PreferencesManager
 import com.cokiri.coinkiri.domain.repository.KakaoLoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,18 +12,38 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val kakaoLoginRepository: KakaoLoginRepository
+    private val kakaoLoginRepository: KakaoLoginRepository,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
 
     private val _loginUiState = MutableStateFlow<LoginUiState>(LoginUiState.Initial)
     val loginUiState = _loginUiState.asStateFlow()
-    private fun kakaoLoginSuccess() { _loginUiState.tryEmit(LoginUiState.LogInSuccess) }
-    private fun kakaoLoginFail() { _loginUiState.tryEmit(LoginUiState.LogInFail) }
-    private fun initial() { _loginUiState.tryEmit(LoginUiState.Initial) }
+
+    init {
+        if (preferencesManager.isLoggedIn) {
+            _loginUiState.value = LoginUiState.LogInSuccess
+        }
+    }
+
+    private fun kakaoLoginSuccess() {
+        _loginUiState.tryEmit(LoginUiState.LogInSuccess)
+        preferencesManager.isLoggedIn = true
+    }
+    private fun kakaoLoginFail() {
+        _loginUiState.tryEmit(LoginUiState.LogInFail)
+    }
+    private fun initialLogin() {
+        _loginUiState.tryEmit(LoginUiState.Initial)
+        preferencesManager.isLoggedIn = false
+    }
 
 
-    // 카카오 로그인
+    /**
+     * 카카오 로그인
+     * 카카오 로그인 성공 시 _loginUiState 에 LogInSuccess 상태를 전달
+     * 카카오 로그인 실패 시 _loginUiState 에 LogInFail 상태를 전달
+     */
     fun kakaoLogin() {
         _loginUiState.value = LoginUiState.Loading
         kakaoLoginRepository.login(
@@ -31,11 +52,15 @@ class LoginViewModel @Inject constructor(
         )
     }
 
-    // 카카오 로그아웃
+    /**
+     * 카카오 로그아웃
+     * 카카오 로그아웃 성공 시 _loginUiState 에 Initial 상태를 전달
+     */
     fun kakaoLogout() {
         viewModelScope.launch {
             kakaoLoginRepository.logout()
-            initial()
+            initialLogin()
         }
     }
+
 }
