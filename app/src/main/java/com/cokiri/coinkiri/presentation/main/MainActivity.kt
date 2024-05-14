b8bbb0312eb20001a0ad2402d3ae37e6963c603d
wjdrjs00 <kimjk3409@gmail.com>
2024-05-15 07:40:22 +0900

    #7 ✨ feat : LogInNavGraph구현 하여 MainActivity에서 로그인 상태에 따른 화면전환 구현

--- app/src/main/java/com/cokiri/coinkiri/presentation/main/MainActivity.kt
+++ app/src/main/java/com/cokiri/coinkiri/presentation/main/MainActivity.kt
@@ -4,22 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.cokiri.coinkiri.presentation.login.KakaoViewModel
-import com.cokiri.coinkiri.presentation.login.LoginScreen
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.rememberNavController
+import com.cokiri.coinkiri.presentation.login.LoginUiState
+import com.cokiri.coinkiri.presentation.login.LoginViewModel
+import com.cokiri.coinkiri.ui.navigation.LogInNavGraph
+import com.cokiri.coinkiri.ui.navigation.MainGraph
 import com.cokiri.coinkiri.ui.theme.CoinkiriandroidTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val kakaoViewModel: KakaoViewModel by viewModels()
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CoinkiriandroidTheme {
-            //LoginScreen(kakaoViewModel)
-                MainScreen()
+
+                val loginUiState by loginViewModel.loginUiState.collectAsState()
+                val navController = rememberNavController()
+
+                when (loginUiState) {
+                    is LoginUiState.LogInSuccess -> MainGraph(navController, loginViewModel)
+                    else -> LogInNavGraph(navController, loginViewModel)
+                }
             }
         }
     }
