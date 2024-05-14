34cb16c89adcb7ae8a2efbde265a8446c23ceb4b
wjdrjs00 <kimjk3409@gmail.com>
2024-05-15 07:42:35 +0900

    ui : 앱 MainColor 등록

--- app/src/main/java/com/cokiri/coinkiri/ui/theme/Color.kt
+++ app/src/main/java/com/cokiri/coinkiri/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val CoinkiriBlack = Color(0xFF1C1B1F)       // 앱에서 전체적으로 사용할 검은색
+val CoinkiriPointGreen = Color(0xFF829F8A)
+val CoinkiriGreen = Color(0xFFE0E5E0)
+val CoinkiriBackground = Color(0xFFF8F8F8)  // 앱의 배경 색
+
