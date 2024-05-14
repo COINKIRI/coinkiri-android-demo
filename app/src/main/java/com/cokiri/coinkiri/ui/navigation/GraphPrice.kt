13bbfe5fc87e554f153cbb8128e7509b69afa80a
wjdrjs00 <kimjk3409@gmail.com>
2024-05-14 21:13:06 +0900

    #6 ♻️ refactor : 각 스크린 별로 Navigation 구현

---
+++ app/src/main/java/com/cokiri/coinkiri/ui/navigation/GraphPrice.kt
@@ -0,0 +1,15 @@
+package com.cokiri.coinkiri.ui.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.cokiri.coinkiri.presentation.price.PriceScreen
+import com.cokiri.coinkiri.util.PRICE
+
+fun NavGraphBuilder.priceNavGraph(
+    navController: NavHostController
+) {
+    composable(PRICE) {
+        PriceScreen()
+    }
+}
