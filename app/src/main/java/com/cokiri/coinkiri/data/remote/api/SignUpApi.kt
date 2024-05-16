89a78df78bbe230d4fffdaae4f40c63d726b8210
wjdrjs00 <kimjk3409@gmail.com>
2024-05-16 09:49:15 +0900

    #12 ✨ feat : api 인터페이스 header추가

--- app/src/main/java/com/cokiri/coinkiri/data/remote/api/SignUpApi.kt
+++ app/src/main/java/com/cokiri/coinkiri/data/remote/api/SignUpApi.kt
@@ -2,10 +2,12 @@ package com.cokiri.coinkiri.data.remote.api
 
 import com.cokiri.coinkiri.data.remote.model.LoginRequest
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface SignUpApi {
 
+    @Headers("Content-Type: application/json")
     @POST("/api/v1/auth/signup")
     suspend fun signUpUser(@Body loginRequest: LoginRequest)
 
