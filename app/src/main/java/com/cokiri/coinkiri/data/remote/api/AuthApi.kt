47ba074404c77b83a870d0907f03a5810d19a8c6
wjdrjs00 <kimjk3409@gmail.com>
2024-05-18 17:32:16 +0900

    #15 ✨ feat : AuthApi에 logoutUser 추가

--- app/src/main/java/com/cokiri/coinkiri/data/remote/api/AuthApi.kt
+++ app/src/main/java/com/cokiri/coinkiri/data/remote/api/AuthApi.kt
@@ -3,6 +3,7 @@ package com.cokiri.coinkiri.data.remote.api
 import com.cokiri.coinkiri.data.remote.model.SignUpRequest
 import com.cokiri.coinkiri.data.remote.model.SignUpResponse
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
@@ -12,5 +13,8 @@ interface AuthApi {
     @POST("/api/v1/auth/signup")
     suspend fun signUpUser(@Body request: SignUpRequest) : SignUpResponse
 
+    @Headers("Content-Type: application/json")
+    @POST("/api/v1/auth/logout")
+    suspend fun logoutUser(@Header("Authorization") accessToken: String)
 
 }
