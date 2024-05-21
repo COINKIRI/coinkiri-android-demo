683c15483c15f2a290d5515116f6f675c1b19476
wjdrjs00 <kimjk3409@gmail.com>
2024-05-21 12:29:44 +0900

    #20 ✨ feat : 로그인후 해당유저 정보를 받아오는 로직 구현

--- app/src/main/java/com/cokiri/coinkiri/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/cokiri/coinkiri/data/repository/UserRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.cokiri.coinkiri.data.repository
 import android.util.Log
 import com.cokiri.coinkiri.data.remote.PreferencesManager
 import com.cokiri.coinkiri.data.remote.api.AuthApi
+import com.cokiri.coinkiri.data.remote.model.MemberInfo
 import com.cokiri.coinkiri.data.remote.model.SignUpRequest
 import com.cokiri.coinkiri.domain.repository.UserRepository
 import javax.inject.Inject
@@ -15,7 +16,9 @@ class UserRepositoryImpl @Inject constructor(
     private val preferencesManager: PreferencesManager
 ) : UserRepository {
 
-    companion object { private const val TAG = "UserRepositoryImpl" }
+    companion object {
+        private const val TAG = "UserRepositoryImpl"
+    }
 
     override suspend fun signUpUser(accessToken: String, socialType: String): Result<Unit> {
         return try {
@@ -27,13 +30,18 @@ class UserRepositoryImpl @Inject constructor(
             preferencesManager.saveAccessToken(accessToken)     // 액세스 토큰 저장
             preferencesManager.saveRefreshToken(refreshToken)   // 리프레시 토큰 저장
 
-            Log.i(TAG, "회원가입 성공, " +
-                    "accessToken : $accessToken, " +
-                    "refreshToken : $refreshToken ")
+            Log.i(
+                TAG, "회원가입 성공, " +
+                        "accessToken : $accessToken, " +
+                        "refreshToken : $refreshToken "
+            )
+            getMemberInfo()?.let { member ->
+                Log.d(TAG, "사용자 정보: $member")
+            }
 
             Result.success(Unit)
 
-        } catch (error : Exception) {
+        } catch (error: Exception) {
             Log.e(TAG, "회원가입 실패", error)
             Result.failure(error)
         }
@@ -59,6 +67,38 @@ class UserRepositoryImpl @Inject constructor(
             Result.failure(error)
         }
     }
+
+
+    private suspend fun getMemberInfo(): MemberInfo? {
+        return try {
+            val accessToken = preferencesManager.getAccessToken()
+            if (accessToken.isNullOrEmpty()) {
+                Log.e(TAG, "토큰이 없어 사용자 정보를 가져올 수 없습니다.")
+                null
+            } else {
+                val response = authApi.getMemberInfo("Bearer $accessToken")
+                if (response.code == "OK") {
+                    // 응답이 성공적으로 받아졌을 때, 결과를 UserInfo 객체로 변환하여 반환합니다.
+                    val result = response.result
+                    MemberInfo(
+                        id = result.id,
+                        nickname = result.nickname,
+                        exp = result.exp,
+                        level = result.level,
+                        mileage = result.mileage,
+                        pic = result.pic
+                    )
+                } else {
+                    Log.e(TAG, "서버 응답 오류: ${response.message}")
+                    null
+                }
+            }
+        } catch (error: Exception) {
+            Log.e(TAG, "사용자 정보 가져오기 실패", error)
+            null
+        }
+    }
+
 
     override fun isLoggedIn(): Boolean {
         return !preferencesManager.getAccessToken().isNullOrEmpty()
