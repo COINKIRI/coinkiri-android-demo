6a6c249179a18f678ab0398150270b1cecd37b6d
wjdrjs00 <kimjk3409@gmail.com>
2024-05-21 12:35:44 +0900

    #20 ✨ feat : 서버로부터 응답을 받기한 MemberResponse 구현

---
+++ app/src/main/java/com/cokiri/coinkiri/data/remote/model/MemberResponse.kt
@@ -0,0 +1,21 @@
+package com.cokiri.coinkiri.data.remote.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class MemberResponse(
+    @Json(name = "code") val code: String,
+    @Json(name = "message") val message: String,
+    @Json(name = "result") val result: MemberInfo
+)
+
+@JsonClass(generateAdapter = true)
+data class MemberInfo(
+    val id : Long,
+    val nickname : String,
+    val exp : Int,
+    val level : Int,
+    val mileage : Int,
+    val pic : String?
+)
